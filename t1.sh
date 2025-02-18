9d5db5bf2435906ce7d0b02ea89fb1ebd06c202f
ajaypatil9325 <ajaypatil9325@gmail.com>
2025-02-18 17:53:32 +0000

    third commit

--- t1.sh
+++ t1.sh
@@ -15,10 +15,3 @@ This is a shell script to take backups
 can also be used with cron
 help
 
-source_dir="/home/ubuntu/devops-zero-to-hero/scripts"
-destination_dir="/home/ubuntu/devops-zero-to-hero/backups"
-
-function create_backup {
-	timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
-
-	backup_dir="${destination_dir}/backup_${timestamp}"
