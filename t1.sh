ae6d7a784ab99ab665156e52f3b5cee216cbbdfd
ajaypatil9325 <ajaypatil9325@gmail.com>
2025-02-18 17:49:15 +0000

    second

--- t1.sh
+++ t1.sh
@@ -8,4 +8,17 @@ echo "we are learning git"
 
 date
 
-whoami
+whoami 
+#!/bin/bash
+<< help
+This is a shell script to take backups
+can also be used with cron
+help
+
+source_dir="/home/ubuntu/devops-zero-to-hero/scripts"
+destination_dir="/home/ubuntu/devops-zero-to-hero/backups"
+
+function create_backup {
+	timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+
+	backup_dir="${destination_dir}/backup_${timestamp}"
